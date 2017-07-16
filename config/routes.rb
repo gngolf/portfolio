281dede4408891f2870c4329245bce2f11b8ca19
Ubuntu <ubuntu@rails-dev-box>
2017-07-16 20:56:03 +0000

    added other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root to: 'main#index'
-  get 'main/index' => ''
+  get '/index' => 'main#index'
+  get '/golf' => 'main#golf'
+  get '/adventure' => 'main#adventure'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
